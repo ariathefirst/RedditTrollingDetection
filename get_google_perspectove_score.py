ab5228d568b7c406b3ee40e1ee810074923cb5b7
ariathefirst <ariang@ucdavis.edu>
2018-05-30 08:41:50 -0700

    got pscores for 3 files

--- get_google_perspectove_score.py
+++ get_google_perspectove_score.py
@@ -8,9 +8,6 @@ from googleapiclient import discovery
 import sys
 
 def runPerspective(str):
-	# print("what?\n")
-	# print(str)
-	# print("printed str")
 	API_KEY='AIzaSyAlCwhKJ0C8n4eFM-ioPC5-MCFYy4P-TT8'
 
 	# Generates API client object dynamically based on service name and version.
@@ -28,34 +25,36 @@ def runPerspective(str):
 	except googleapiclient.errors.HttpError as e:
 		return "Error!"	
 
-	
-
 reddit = praw.Reddit(client_id='OlM6d2hKSrhbkw',
                      client_secret='w3kllzs-03WScaa9DFMHvRdTnQg',
                      password='WebW0rld',
                      user_agent='script by /u/sravyadivakarla123',
                      username='sravyadivakarla123')
 
-i = 0
-pScore = "pineapplecharmPscore.csv"
-with open(pScore, "w+") as w:
-	writer = csv.writer(w, delimiter = ",")
-	with open("pineapplecharm.csv", 'rU') as f:
-		comment_res = csv.reader(f, delimiter = ",")
-		for item in comment_res: # for each line
-			pscore = runPerspective(item[0])
-			# if i == 5:
-			# 	break	
-			if i == 0:
-				item.append("pscore") 
-				print(item[7])
-			else:
-				item.append(pscore)
-				print(item[7])
-			i = i + 1
-			print("item is")
-			print(item)
-			writer.writerow(item)
-print("done")
+def getPerspective(res_file, file):
+	i = 0
+	with open(res_file, "w+") as w:
+		writer = csv.writer(w, delimiter = ",")
+		with open(file, 'rU') as f:
+			comment_res = csv.reader(f, delimiter = ",")
+			for item in comment_res: # for each row
+				print(i)
+				if i == 0:
+					item.append("pscore") # adds pscore attribute
+				else:
+					pscore = runPerspective(item[0]) # gets pscore of comment
+					item.append(pscore) # appends pscore to end of row
+				i = i + 1
+				writer.writerow(item) # write cur row to output file res_file
+	print("done")
+
+def main(): # change this to a loop to call all files within a bigQueryData dir
+	getPerspective("pineapplecharmPscore.csv", "bigQueryData/pineapplecharm.csv")
+	getPerspective("sosorrynonamePscore.csv", "bigQueryData/sosorrynoname.csv")
+	getPerspective("parent_id=t3_20myzoPscore.csv", "bigQueryData/parent_id=t3_20myzo.csv")
+
+if __name__== "__main__":
+	main()
+
 
 
